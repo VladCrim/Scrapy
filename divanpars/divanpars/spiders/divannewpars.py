181f56c615cb4b70a553d17c1a1bf69cb53ba37d
Владимир Мережко <Merezhko11@rambler.ru>
2025-03-24 00:55:27 +0300

    add

---
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/search?ProductSearch%5Bname%5D=освещение&no_cache=1"]
+
+    def parse(self, response):
+        lights = response.css('div.WdR1o')
+        for light in lights:
+            yield {
+                'name': light.css('div.lsooF span::text').get(),
+                'price': light.css('div.pY3d2 span::text').get(),
+                'url': light.css('a').attrib['href']
+            }
